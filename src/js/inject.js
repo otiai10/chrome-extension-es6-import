120751243983ecf940a4b084d70d35ea694b6c16
Hiromu Ochiai <otiai10@gmail.com>
2018-03-27 21:10:38 +0900

    Remove unnecessary log

--- src/js/inject.js
+++ src/js/inject.js
@@ -1,6 +1,5 @@
 (() => {
   const src = chrome.extension.getURL('src/js/content.js');
-  console.log(src);
   const script = document.createElement('script');
   script.setAttribute('src', src);
   script.setAttribute('type', 'module');
